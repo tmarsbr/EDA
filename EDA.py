3c03c33c3349bd5eb0d9713453293fce5c625506
tmars <tiagomars233@gmail.com>
2025-01-21 17:21:57 -0300

    test

---
+++ EDA.py
@@ -0,0 +1,13 @@
+# %%
+# EDA spotipy data
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %%
+df = pd.read_csv(r'C:\Users\tiago\OneDrive\Área de Trabalho\meus cursos\nao é nada\Spotify Most Streamed Songs.csv')
+df.head()
+# %%
+df.shape
+# %%
